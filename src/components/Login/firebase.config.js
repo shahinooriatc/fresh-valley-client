278b2f27777775137ad0cf584eb79a37f8e539ca
shahinooriact <shahinooriact@gmail.com>
2021-06-15 11:36:28 +0600

    firebase config worked properly

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,9 +1,17 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDGUwZp6dcpLCd_0M4_YqISD4NV4AF2jfo",
+//   authDomain: "fir-recap42-5.firebaseapp.com",
+//   projectId: "fir-recap42-5",
+//   storageBucket: "fir-recap42-5.appspot.com",
+//   messagingSenderId: "701689522201",
+//   appId: "1:701689522201:web:1b598ad8af2cc43fcea288"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDGUwZp6dcpLCd_0M4_YqISD4NV4AF2jfo",
-  authDomain: "fir-recap42-5.firebaseapp.com",
-  projectId: "fir-recap42-5",
-  storageBucket: "fir-recap42-5.appspot.com",
-  messagingSenderId: "701689522201",
-  appId: "1:701689522201:web:4c67f7e6810096cacea288"
+  apiKey: "AIzaSyD7GUzeQGfGD52iImeMfCR-rBCVOQcogPQ",
+  authDomain: "ema-jhom-simple43.firebaseapp.com",
+  projectId: "ema-jhom-simple43",
+  storageBucket: "ema-jhom-simple43.appspot.com",
+  messagingSenderId: "438360107786",
+  appId: "1:438360107786:web:22a1de6c0006a79c1e28f3"
 };
 export default firebaseConfig;
